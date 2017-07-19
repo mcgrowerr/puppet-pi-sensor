286b202ddbf556e1fe1cf306d4745ec49c9df9e2
mcgrowerr <mcgrowerr@gmail.com>
2017-07-19 13:10:25 -0700

    Adding telegraf settings

--- modules/role/manifests/growtent.pp
+++ modules/role/manifests/growtent.pp
@@ -4,4 +4,9 @@ class role::growtent {
     include dht22
     include pilights::disable_network_led
 
+    class { 'telegraf':
+        influxdb_url => 'http://10.10.105.106:8086',
+        influx_db    => 'telegraf',
+    }
+
 }
