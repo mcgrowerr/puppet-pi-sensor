e640894f05497464d29689a1f9a532a881518128
mcgrowerr <mcgrowerr@gmail.com>
2017-07-19 12:32:55 -0700

    Disable network leds

---
+++ modules/pilights/manifests/disable_network_led.pp
@@ -0,0 +1,23 @@
+# This class disables the network led on the pi
+class pilights::disable_network_led {
+    file { '/usr/local/bin/disable_network_led':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        content => template('pilights/disable_network_led.erb'),
+    }
+
+    file { '/etc/systemd/system/disable_network_led.service':
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        content => template('pilights/disable_network_led.service.erb')
+        require => File['/usr/local/bin/disable_network_led'],
+    }
+
+    service { 'disable_network_led':
+        ensure  => 'started',
+        enable  => true,
+        require => File['/etc/systemd/system/disable_network_led.service'],
+    }
+}
